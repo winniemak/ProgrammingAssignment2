0c20898ae682ecfda2135b4b98ef66e970b60800
winniemak <winniemak@gmail.com>
2018-05-07 16:23:06 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix vector and saves in the private variable x
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    #intialize the inverse matrix to NULL at makeCacheMatrix construction time
     m <- NULL
+    
+    #use <<- operator to set the value of x and m in specific set environment scope
     set <- function(y) {
     x <<- y
     m <<- NULL
   }
+    
+    #just a stardard get function that return value of fuction x
   get <- function() x
+    
+    # set the inverse matrix 
   setsolve <- function(solve) m <<- solve
+    
+    # return/get the inverse matrix
   getsolve <- function() m
+    
+    # create a list of operations
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
